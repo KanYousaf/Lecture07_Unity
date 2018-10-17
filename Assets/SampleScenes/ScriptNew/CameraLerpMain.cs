024b1136a849a279ab3366c9ae74185112b98d6f
Kanwal Yousaf <kanwal.yousaf@uettaxila.edu.pk>
2018-10-17 12:02:15 +0500

    Lecture07

---
+++ Assets/SampleScenes/ScriptNew/CameraLerpMain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLerpMain : MonoBehaviour {
+
+    private Transform mTargetTransform;
+    private Vector3 offset;
+    private float smoothing = 3.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = new Vector3(-5.0f, 0.0f, 0.0f);
+        this.gameObject.SetActive(false);
+        mTargetTransform = GameObject.Find("Character").transform;
+    }
+
+
+    void LateUpdate()
+    {
+        Vector3 targetVectorPosition = mTargetTransform.position + offset;
+        transform.LookAt(mTargetTransform);
+        transform.position = Vector3.Lerp(transform.position, targetVectorPosition, smoothing * Time.deltaTime);
+    }
+}
